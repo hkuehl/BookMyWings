dce16e9f7629ea2d9b41a436dbce7fcfef81a2cc
hkuehl <henry.kuehl@prodyna.com>
2014-06-26 22:41:27 +0200

    Embedded WildFly-configuration for travis using Arquillian and some ant-run stuff

--- bmw-integration-test/src/test/java/com/prodyna/bmw/server/Deployment.java
+++ bmw-integration-test/src/test/java/com/prodyna/bmw/server/Deployment.java
@@ -13,10 +13,19 @@ public class Deployment {
 
 	@org.jboss.arquillian.container.test.api.Deployment
 	public static WebArchive createDeployment() {
+
+		// File[] mpc = Maven.resolver().loadPomFromFile("pom.xml")
+		// .importCompileAndRuntimeDependencies().resolve()
+		// .withTransitivity().asFile();
+		// for (File f : mpc) {
+		// System.out.println("Found dependency: " + f);
+		// }
+
 		WebArchive wa = ShrinkWrap.create(WebArchive.class, "test.war");
 		wa.addPackages(true, "com.prodyna.bmw.server");
 		wa.addAsResource("META-INF/persistence.xml");
 		wa.addAsResource("META-INF/beans.xml");
+		// wa.addAsLibraries(mpc);
 		System.out.println(wa.toString(true));
 		return wa;
 	}
